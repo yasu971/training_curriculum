f4c2b26763c3fd02bf45d66e24f646fe5ab90fcb
yasu971 <yasuishii1421@gmail.com>
2020-11-02 16:59:14 +0900

    Issue1

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,7 +34,9 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      # days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      # ↑ハッシュロケット(旧)↓シンボル型(新)
+      days = { :month : (@todays_date + x).month, :date : (@todays_date+x).day, :plans : today_plans}
       @week_days.push(days)
     end
 
